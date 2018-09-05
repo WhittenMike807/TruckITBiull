98911ac8abb9d1b00f75152b22228842ebaeaeb2
Michael Whittenburg <mhittenburg@gmail.com>
2018-09-05 10:01:31 -0400

    ghjhjgj

--- build/main.js
+++ build/main.js
@@ -63,19 +63,112 @@ var HomePage = /** @class */ (function () {
     HomePage.prototype.loadArray = function () {
         this.slides = [
             {
-                image: "assets/imgs/0_bg_app_c.png"
+                orderId: 1,
+                image: "assets/imgs/1_intro_screen_c.png"
             },
             {
-                image: "assets/imgs/1_sign_in_c.png"
+                orderId: 2,
+                image: "assets/imgs/2_before_you_begin.png"
             },
             {
-                image: "assets/imgs/2_trucks_c.png"
+                orderId: 3,
+                image: "assets/imgs/3_how_apps_work_c.png"
             },
             {
-                image: "assets/imgs/2_sel_trucks.png"
+                orderId: 4,
+                image: "assets/imgs/4_bg_app_c.png"
             },
             {
-                image: "assets/imgs/3_data_share_c.png"
+                orderId: 5,
+                image: "assets/imgs/5_signin.png"
+            },
+            {
+                orderId: 6,
+                image: "assets/imgs/6_trucks_c.png"
+            },
+            {
+                orderId: 7,
+                image: "assets/imgs/7_sel_truck_c.png"
+            },
+            {
+                orderId: 8,
+                image: "assets/imgs/8_data_share_c.png"
+            },
+            {
+                orderId: 9,
+                image: "assets/imgs/9_confirm_truck_c.png"
+            },
+            {
+                orderId: 10,
+                image: "assets/imgs/10_changetruck_c.png"
+            },
+            {
+                orderId: 11,
+                image: "assets/imgs/11_view_job_c.png"
+            },
+            {
+                orderId: 12,
+                image: "assets/imgs/12_job_details.png"
+            },
+            {
+                orderId: 13,
+                image: "assets/imgs/13_get_directions_c.png"
+            },
+            {
+                orderId: 14,
+                image: "assets/imgs/14_google_maps_c.png"
+            },
+            {
+                orderId: 15,
+                image: "assets/imgs/15_get_loaded_c.png"
+            },
+            {
+                orderId: 16,
+                image: "assets/imgs/16_material_loaded.png"
+            },
+            {
+                orderId: 17,
+                image: "assets/imgs/17_materials_detail_c.png"
+            },
+            {
+                orderId: 18,
+                image: "assets/imgs/18_proceed_to_drop_off_c.png"
+            },
+            {
+                orderId: 19,
+                image: "assets/imgs/19_complete_c.png"
+            },
+            {
+                orderId: 20,
+                image: "assets/imgs/20_next_job_c.png"
+            },
+            {
+                orderId: 21,
+                image: "assets/imgs/21_few_more_things.png"
+            },
+            {
+                orderId: 22,
+                image: "assets/imgs/22_show_tickets_c.png"
+            },
+            {
+                orderId: 23,
+                image: "assets/imgs/23_ticket.png"
+            },
+            {
+                orderId: 24,
+                image: "assets/imgs/24_device_settings.png"
+            },
+            {
+                orderId: 25,
+                image: "assets/imgs/25_data_on.png"
+            },
+            {
+                orderId: 26,
+                image: "assets/imgs/26_support.png"
+            },
+            {
+                orderId: 27,
+                image: "assets/imgs/27_get_started_c.png"
             }
         ];
     };
